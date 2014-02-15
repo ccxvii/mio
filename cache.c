5c3db15ad4c78bdbe70194b480e49e8cb3f3c519
Tor Andersson <tor@ccxvii.net>
2014-02-15 11:55:35 +0100

    Simplify and optimize cache tree.

--- cache.c
+++ cache.c
@@ -24,41 +24,39 @@ static struct cache *make_node(const char *key, void *value)
 
 void *lookup(struct cache *node, const char *key)
 {
-	if (node && node != &sentinel) {
-		int c = strcmp(key, node->key);
-		if (c == 0)
-			return node->value;
-		else if (c < 0)
-			return lookup(node->left, key);
-		else
-			return lookup(node->right, key);
+	if (node) {
+		while (node != &sentinel) {
+			int c = strcmp(key, node->key);
+			if (c == 0)
+				return node->value;
+			else if (c < 0)
+				node = node->left;
+			else
+				node = node->right;
+		}
 	}
 	return NULL;
 }
 
 static struct cache *skew(struct cache *node)
 {
-	if (node->level != 0) {
-		if (node->left->level == node->level) {
-			struct cache *save = node;
-			node = node->left;
-			save->left = node->right;
-			node->right = save;
-		}
-		node->right = skew(node->right);
+	if (node->left->level == node->level) {
+		struct cache *save = node;
+		node = node->left;
+		save->left = node->right;
+		node->right = save;
 	}
 	return node;
 }
 
 static struct cache *split(struct cache *node)
 {
-	if (node->level != 0 && node->right->right->level == node->level) {
+	if (node->right->right->level == node->level) {
 		struct cache *save = node;
 		node = node->right;
 		save->right = node->left;
 		node->left = save;
 		node->level++;
-		node->right = split(node->right);
 	}
 	return node;
 }
@@ -74,9 +72,8 @@ struct cache *insert(struct cache *node, const char *key, void *value)
 		node = skew(node);
 		node = split(node);
 		return node;
-	} else {
-		return make_node(key, value);
 	}
+	return make_node(key, value);
 }
 
 static void print_cache_imp(struct cache *node, int level)
